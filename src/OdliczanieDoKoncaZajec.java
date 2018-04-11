a5b858d3fb39b9f93cd6fe634e0829b53b9e2047
FilipCysEk <krzysiek.swal@gmail.com>
2018-04-11 15:13:56 +0200

    Klasa odliczająca Poprawki i możliwość uruchomienia

--- src/OdliczanieDoKoncaZajec.java
+++ src/OdliczanieDoKoncaZajec.java
@@ -4,12 +4,29 @@ import java.time.LocalTime;
 import java.util.Scanner;
 
 
+class Main {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        OdliczanieDoKoncaZajec Odl = new OdliczanieDoKoncaZajec();
+        System.out.println(Odl.test());
+        System.out.println("Hello World!2");
+        Odl.run();
+
+
+    }
+
+
+}
 
 public class OdliczanieDoKoncaZajec {
     private static Object LOCK = new Object();
 
-    public static void main(String[] args) throws IOException {
+    public String test(){
+        return "Lalalala";
+    }
+
+    public void run() {
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Hello World!");
@@ -34,9 +51,11 @@ public class OdliczanieDoKoncaZajec {
             zostalo = Duration.between(teraz, koniec);
 
             //System.out.println("Do koĹ„ca zajÄ™c zostaĹ‚o: ");
-            System.out.print(zostalo.toMinutes() + ":" + zostalo.getSeconds());
+            //System.out.print(zostalo.toMinutes() + ":" + zostalo.getSeconds());
+            System.out.print(zostalo.getSeconds()/60 + ":" + zostalo.getSeconds()%60);
             System.out.println();
         }
+        return;
 
         //System.out.println(test);
     }
