8e4784a7dafbde462c58399f295bd0ca8591b418
Nimander <szymanskimichal57@gmail.com>
2018-05-29 13:55:40 +0200

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import view.MainWindow;
+
+public class App {
+	public static void main(String[] args) {
+		MainWindow window = new MainWindow();
+	}
+}
---
+++ src/main/java/view/MainWindow.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+	public MainWindow() {
+		super("janekcoin na zaliczenie");
+		setSize(300, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		String login = getLogin();
+		JPanel pane = new JPanel();
+		JButton buyJanek = new JButton("Kup Janka");
+		pane.add(buyJanek);
+		JButton sellJanek = new JButton("Sprzedaj Janka");
+		pane.add(sellJanek);
+		ImageIcon ii = new ImageIcon("src/main/resources/janek.png");
+		JLabel lbl = new JLabel(ii);
+		pane.add(lbl);
+		add(pane);
+
+		setVisible(true);
+	}
+
+	private String getLogin() {
+		return JOptionPane.showInputDialog(null, "Username:");
+	}
+}
