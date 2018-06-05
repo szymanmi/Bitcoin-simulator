edf76c78a146345ff674fdbd4018fe37668dad0d
Nimander <szymanskimichal57@gmail.com>
2018-06-05 14:31:13 +0200

    added actionListeners in MainWindow for two basic buttons

--- src/main/java/view/MainWindow.java
+++ src/main/java/view/MainWindow.java
@@ -4,31 +4,25 @@ import repository.Login;
 
 import javax.swing.*;
 
+
 import static repository.Bitcoin.getBitcoinValue;
 
 public class MainWindow extends JFrame {
-
 	public MainWindow() {
 		super("janekcoin na zaliczenie");
 		setSize(350, 350);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 
-		String userName, password;
-		do {
-			userName = getLogin();
-			password = getPassword();
-		}
-		while (!Login.isLoginDataCorrect(userName, password));
 
-
-
+		welcomeWindow();
 
 		String price = String.valueOf(getBitcoinValue());
 		JLabel pricelbl = new JLabel("Aktualna cena janka to: " + price);
 		JPanel pane = new JPanel();
-		JButton buyJanek = new JButton("Kup Janka");
-		pane.add(buyJanek);
+
+		JButton buyButton = new JButton("Kup Janka");
+		pane.add(buyButton);
 		JButton sellJanek = new JButton("Sprzedaj Janka");
 		pane.add(sellJanek);
 		ImageIcon ii = new ImageIcon("src/main/resources/janek.png");
@@ -36,14 +30,42 @@ public class MainWindow extends JFrame {
 		pane.add(pricelbl);
 		pane.add(lbl);
 		add(pane);
+		remove(pane);
 
+		setVisible(true);
+	}
+
+	private void welcomeWindow() {
+		JPanel panel = new JPanel();
+		JLabel welcomeMessage = new JLabel("Witaj w BitJanku (nazwa tymczasowa)");
+		JButton loginButton = new JButton("Zaloguj");
+		JButton registerButton = new JButton("Zarejestruj");
+		panel.add(welcomeMessage);
+		panel.add(loginButton);
+		panel.add(registerButton);
+
+		loginButton.addActionListener(event -> login());
+		registerButton.addActionListener(event -> register());
+		add(panel);
 		setVisible(true);
 	}
 
 	private String getLogin() {
 		return JOptionPane.showInputDialog(null, "Username (janek):");
 	}
+
 	private String getPassword() {
 		return JOptionPane.showInputDialog(null, "Password (janek):");
 	}
+
+	private String register() {
+		return JOptionPane.showInputDialog(null, "rejestracja");
+	}
+
+	private void login() {
+		getLogin();
+		getPassword();
+	}
+
+
 }
