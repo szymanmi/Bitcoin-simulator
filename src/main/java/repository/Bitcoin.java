7ab1f55b063698eafa6d6003b243328e89795199
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2018-06-19 12:26:05 +0200

    bit

--- src/main/java/repository/Bitcoin.java
+++ src/main/java/repository/Bitcoin.java
@@ -1,16 +1,35 @@
 package repository;
 
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
 
 public class Bitcoin {
 	public static double getBitcoinValue() {
-		/*
-		TODO:
-			value pobierana z internetu
-		 */
-		//Random rand = new Random();
+		String url = "https://bitmarket24.pl/api/BTC_PLN/status.json";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		// optional default is GET
+		con.setRequestMethod("GET");
+		//add request header
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		int responseCode = con.getResponseCode();
 
-		//return rand.nextDouble();
-		return 0.5;
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) { response.append(inputLine); }
+		in.close();
+
+		//Wypisanie stringa (zawartości strony)
+		//System.out.println(response.toString());
+
+		//Czytanie zawartości JSON i wypisanie je
+		JSONObject myResponse = new JSONObject(response.toString());
+
+		return Double.parseDouble(myResponse.getString("last"));
 	}
 }
